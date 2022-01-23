c341192da6a1c6f8f2c78e6b70cd304c6d2c48ed
氏原未結 <ujiharamiyu@MacBook.local>
2022-01-23 15:04:31 +0900

    nabvarを更新

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path notice: "ログアウトしました"
+    redirect_to root_path notice: "ログアウトしました"
   end
 end

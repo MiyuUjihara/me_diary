e22b5c049faceaf86c4a66578bc096d717c09d3d
氏原未結 <ujiharamiyu@MacBook.local>
2022-02-27 13:55:33 +0900

    admin/navbarを編集

--- app/javascript/packs/admin_navbar.js
+++ app/javascript/packs/admin_navbar.js
@@ -3,17 +3,22 @@ $(document).on('turbolinks:load', ()=> {
   const URL = location.href
 
   const TargetUrl = {
-    dashbord: "/admin/columns",
+    columnindex: "/admin/columns",
     column: "/admin/columns/new",
-    todo: "/admin/todos/new"
+    todo: "/admin/todos/new",
+    todoindex: "/admin/todos",
+    dashboard: "/admin"
   }
 
   if (URL.includes(TargetUrl.column)) {
     $('.admin_choice2').addClass("select")
-  } else if (URL.includes(TargetUrl.dashbord)) {
-    $('.admin_choice').addClass("select")
-  } else if (URL.includes(TargetUrl.todo)) {
+  } else if (URL.includes(TargetUrl.columnindex)) {
     $('.admin_choice3').addClass("select")
+  } else if (URL.includes(TargetUrl.todo)) {
+    $('.admin_choice4').addClass("select")
+  } else if (URL.includes(TargetUrl.todoindex)) {
+    $('.admin_choice5').addClass("select")
+  } else if (URL.includes(TargetUrl.dashboard)) {
+    $('.admin_choice').addClass("select")
   }
-
 });

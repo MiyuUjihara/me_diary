10f458f640e12eddf1d51aa2ec901847c15bb4ea
氏原未結 <ujiharamiyu@MacBook.local>
2022-03-08 20:33:11 +0900

    マイグレーションファイル修正

--- db/migrate/20220308113009_add_status_to_columns.rb
+++ db/migrate/20220308113009_add_status_to_columns.rb
@@ -1,4 +1,4 @@
-class AddStatusToColumn < ActiveRecord::Migration[6.0]
+class AddStatusToColumns < ActiveRecord::Migration[6.0]
   def change
     add_column :columns, :status, :integer, null: false, default: 0, comment: "ステータス"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_084403) do
+ActiveRecord::Schema.define(version: 2022_03_08_113009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
